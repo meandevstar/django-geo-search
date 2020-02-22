2972ddc15236d7bc8dfda2643d08a9f7e8eb00a8
meandevstar <aimoko1067@yahoo.com>
2020-02-21 22:09:17 -0600

    feat(ListAPI | WIP): add list api with hardcoded user location

--- nearbyshops/shops/views.py
+++ nearbyshops/shops/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.gis.geos import fromstr, Point
+from django.contrib.gis.db.models.functions import Distance
+from .models import Shop
 
-# Create your views here.
+longitude = 54.967302
+latitude = 37.313515
+
+user_location = Point(longitude, latitude, srid=4326)
+
+class ShopListAPIView(generic.ListView):
+    model = Shop
+    context_object_name = 'shops'
+    queryset = Shop.objects.annotate(distance=Distance('location', user_location)) \
+    	.order_by('distance')[0:10]
+    template_name = 'shops/index.html'
